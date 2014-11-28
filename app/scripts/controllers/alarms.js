07e8707c492d9fcc355fae118f9395e8e971905d
gerbil <jurijs.kobecs@gmail.com>
2014-11-28 14:11:04 +0200

    Initial commit

---
+++ app/scripts/controllers/alarms.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dashboardApp.controller:AlarmsCtrl
+ * @description
+ * # AlarmsCtrl
+ * Controller of the dashboardApp
+ */
+angular.module('dashboardApp')
+    .controller('AlarmsCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
